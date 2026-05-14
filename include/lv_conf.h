cc9aa51e20e38f4284131b781a96c36c8ab9af75
tomx <33961528+tomx-sh@users.noreply.github.com>
2026-05-14 13:41:44 +0200

    style

--- include/lv_conf.h
+++ include/lv_conf.h
@@ -3,4 +3,4 @@
 #define LV_CONF_H
 #define LV_COLOR_DEPTH 16
 #define LV_USE_OS LV_OS_NONE
-
+#define LV_FONT_MONTSERRAT_32 1
--- src/main.cpp
+++ src/main.cpp
@@ -65,8 +65,12 @@ void setup() {
       sizeof(drawBuffer),
       LV_DISPLAY_RENDER_MODE_PARTIAL);
 
+  lv_obj_set_style_bg_color(lv_screen_active(), lv_color_black(), 0);
+
   lv_obj_t *label = lv_label_create(lv_screen_active());
   lv_label_set_text(label, "Hello, LVGL 9");
+  lv_obj_set_style_text_color(label, lv_color_white(), 0);
+  lv_obj_set_style_text_font(label, &lv_font_montserrat_32, 0);
   lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
   Serial.println("Display ready");
